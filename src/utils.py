a9c39a9e45fe84468b748c9d019093b65c896df7
BenSturgeon <sturgeonkid@gmail.com>
2022-08-29 11:00:05 +0200

    Completed the forward pass and most of the backward pass, as well as model instantiation.
    Added csv files to gitignore.
    
    Untracked csv files.

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 """utility functions"""
 
+import numpy as np
+
 
 def ReLU(n: float) -> float:
     """rectified linear unit activation function"""
-    return n if n > 0 else 0
+    return np.maximum(n, 0)
 
 
 def ReLU_deriv(n: float) -> int:
-    ...
+    return n > 0
